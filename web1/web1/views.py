c5451524c8e18094babd02da2a4d36a6ea9dbb88
zeap83 <jean_paez@hotmail.es>
2021-06-11 15:12:18 -0500

    Html a mi primera vista con variable
    Se creó una variable donde está el contenido de la primera vista, pero usando la estructura de HTML.

--- web1/web1/views.py
+++ web1/web1/views.py
@@ -2,4 +2,12 @@ from django.http import HttpResponse
 
 def saludo(request): # Nuestra primera vista
 
-    return HttpResponse("Hola profe, esta es nuestra primera vista usando el MVC, en python django se llama Model Template View")
+    documento = """<html>
+    <body>
+    <h1>
+    Hola profe, esta es nuestra primera vista usando el MVC, en python django se llama Model Template View.
+    </h1>
+    </body>
+    </html>"""
+
+    return HttpResponse(documento)
